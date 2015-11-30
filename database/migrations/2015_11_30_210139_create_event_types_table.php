277bd80064bdd244a8e018a5e4150908626e3a4c
Brent Garner <bgarner@gmail.com>
2015-11-30 15:28:33 -0700

    adding migrations for event table

---
+++ database/migrations/2015_11_30_210139_create_event_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_types', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->mediumText('event_type');    
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('event_types', function (Blueprint $table) {
+            Schema::dropIfExists('event_types');
+        });
+    }
+}
