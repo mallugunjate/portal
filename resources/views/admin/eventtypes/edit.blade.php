4bfab4c82d0a237f65642f752f982b0502eeadb9
Brent Garner <bgarner@gmail.com>
2015-12-07 11:50:23 -0700

    wrapping up this feature

--- resources/views/admin/eventtypes/edit.blade.php
+++ resources/views/admin/eventtypes/edit.blade.php
@@ -65,7 +65,7 @@
 
                                         <div class="form-group">
                                             <div class="col-sm-4 col-sm-offset-2">
-                                                <a class="btn btn-white" href="/admin/calendar"><i class="fa fa-close"></i> Cancel</a>
+                                                <a class="btn btn-white" href="/admin/eventtypes"><i class="fa fa-close"></i> Cancel</a>
                                                 <button class="eventtype-edit btn btn-primary" type="submit"><i class="fa fa-check"></i> Create New Event</button>
 
                                             </div>
--- resources/views/admin/eventtypes/index.blade.php
+++ resources/views/admin/eventtypes/index.blade.php
@@ -23,7 +23,7 @@
 
 		<div class="row wrapper border-bottom white-bg page-heading">
                 <div class="col-lg-10">
-                    <h2>Calendar Events</h2>
+                    <h2>Calendar Event Types</h2>
                     <ol class="breadcrumb">
                         <li>
                             <a href="index.html">Home</a>
