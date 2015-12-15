3155bedf3f01596b4b19226d852e4eeddd5942ed
Brent Garner <bgarner@gmail.com>
2015-12-15 16:46:32 -0700

    invididual message view now loading data

--- resources/views/site/communications/message.blade.php
+++ resources/views/site/communications/message.blade.php
@@ -65,23 +65,19 @@
 
 <div class="col-lg-9 animated fadeInRight">
             <div class="mail-box-header">
-                <div class="pull-right tooltip-demo">
+<!--                 <div class="pull-right tooltip-demo">
                     <a href="mail_compose.html" class="btn btn-white btn-sm" data-toggle="tooltip" data-placement="top" title="Reply"><i class="fa fa-reply"></i> Reply</a>
                     <a href="#" class="btn btn-white btn-sm" data-toggle="tooltip" data-placement="top" title="Print email"><i class="fa fa-print"></i> </a>
                     <a href="mailbox.html" class="btn btn-white btn-sm" data-toggle="tooltip" data-placement="top" title="Move to trash"><i class="fa fa-trash-o"></i> </a>
-                </div>
+                </div> -->
                 <h2>
-                    View Message
+                    {{ $communication->subject }}
                 </h2>
                 <div class="mail-tools tooltip-demo m-t-md">
 
-
-                    <h3>
-                        <span class="font-noraml">Subject: </span>Aldus PageMaker including versions of Lorem Ipsum.
-                    </h3>
                     <h5>
-                        <span class="pull-right font-noraml">10:15AM 02 FEB 2014</span>
-                        <span class="font-noraml">From: </span>alex.smith@corporation.com
+                        <span class="pull-right font-noraml">{{ $communication->send_at }}</span>
+                        <span class="font-noraml">From: </span>{{ $communication->sender }}
                     </h5>
                 </div>
             </div>
@@ -89,21 +85,7 @@
 
 
                 <div class="mail-body">
-                    <p>
-                        Hello Jonathan!
-                        <br>
-                        <br>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
-                        took a galley of type and scrambled it to make a type <strong>specimen book.</strong>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It has survived not only five centuries, but also the leap into electronic typesetting, remaining
-                        essentially unchanged.
-                        </p>
-                    <p>
-                        It was popularised in the 1960s with the release <a href="#" class="text-navy">Letraset sheets</a>  containing Lorem Ipsum passages, and more recently with desktop publishing software
-                        like Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                    <p>
-                        There are many variations of passages of <strong>Lorem Ipsum</strong>Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of.
-                    </p>
+                    {!! $communication->body !!}
                 </div>
                     <div class="mail-attachment">
                         <p>
