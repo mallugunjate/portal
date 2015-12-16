f51cd4bc31668d422d6090f35c4518bd481cc320
Brent Garner <bgarner@gmail.com>
2015-12-16 14:38:06 -0700

    writing store number to localStorage

---
+++ public/js/custom/site/storeselector/storeSelector.js
@@ -0,0 +1,54 @@
+const API_DOMAIN = "http://localhost:8888";
+
+$( document ).ready(function() {
+
+	// if( localStorage.getItem('userBanner') && localStorage.getItem('userStoreNumber') ) {
+	// //	window.location="/dashboard";
+	// } else {
+		console.log("check state of banner/store....");
+		console.log("Banner: " + localStorage.getItem('userBanner') );
+		console.log("Store: " + localStorage.getItem('userStoreNumber') );
+
+		getBanners();
+
+		var bannerDropdown = document.getElementById('bannerSelect');
+		var storeDropdown = document.getElementById('storeSelect');
+
+		bannerDropdown.onchange = function() {
+			localStorage.setItem('userBanner', bannerDropdown.options[bannerDropdown.selectedIndex].value);
+			console.log("set the userBanner = " + localStorage.getItem('userBanner') );		
+			getStores( localStorage.getItem('userBanner') );
+		}
+		storeDropdown.onchange = function() {
+			localStorage.setItem('userStoreNumber', storeDropdown.options[storeDropdown.selectedIndex].value);
+			console.log("set the userStoreNumber = " + localStorage.getItem('userStoreNumber') );
+//			window.location="/dashboard";
+		}	
+	// }	
+});
+
+var getBanners = function()
+{
+    var jqxhr = $.getJSON( API_DOMAIN + "/banners", function(json) {
+ 
+    var i=0;
+        $.each(json, function(index, element) {
+            $("#bannerSelect").append("<option value='"+ element.id +"'>"+ element.name +"</option>");
+            i++;
+        });
+    })
+}
+
+var getStores = function(banner)
+{
+	$("#storeSelect").empty();	
+	$("#storeSelect").append("<option>--- Select Your Store ---</option>");
+	var jqxhr = $.getJSON( API_DOMAIN + "/banner/" + banner, function(json) {
+ 
+    var i=0;
+        $.each(json, function(index, element) {
+            $("#storeSelect").append("<option value='"+ element.store_number +"'>"+ element.store_number + " " + element.name +"</option>");
+            i++;
+        });
+    })
+}
---
+++ resources/views/site/storeselector.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Choose your store</title>
+</head>
+<body>
+
+<select id="bannerSelect">
+	<option>--- Select Your Banner ----</option>
+</select>
+
+<select id="storeSelect">
+	
+</select>
+
+</body>
+
+<script type="text/javascript" src="/js/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="/js/custom/site/storeselector/storeSelector.js"></script>
+
+</html>
