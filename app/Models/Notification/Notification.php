86da198c457eeebf26a5dd49f962a7e32a5ce56d
Brent Garner <bgarner@gmail.com>
2016-02-03 16:54:42 -0700

    notificaiton class has getAllNotifications method done

--- app/Models/Notification/Notification.php
+++ app/Models/Notification/Notification.php
@@ -3,16 +3,38 @@
 namespace App\Models\Notification;
 
 use Illuminate\Database\Eloquent\Model;
-use Models\Document\Document
+use App\Models\Document\Document;
+use Carbon\Carbon;
 
 class Notification extends Model
 {
-    public static function getAllNotifications($windowType, $windowSize)
+    public static function getAllNotifications($bannerId, $windowType, $windowSize)
     {
+    	switch($windowType){
+    		case 1:  //by number of days
+    			$dateSince = Carbon::now()->subDays($windowSize)->toDateTimeString();
 
+    			$notifications = Document::where('banner_id', $bannerId)
+    							->where('created_at', '>=', $dateSince)
+    							->orderBy('updated_at', 'desc')
+    							->get();
+    			break;
+    		case 2:  //by number of documents
+    			$notifications = Document::where('banner_id', $bannerId)
+    							->orderBy('updated_at', 'desc')
+    							->take($windowSize)
+    							->get();
+    			break;
+    		default:
+    			$notifications ="";
+    			break;
+    	}
+
+    	return $notifications;
+    	
     }
 
-    public static function getNotificationsByFeature($windowType, $windowSize, $featureId)
+    public static function getNotificationsByFeature($bannerId, $windowType, $windowSize, $featureId)
     {
 
     }
