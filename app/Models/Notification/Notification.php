1d5a86ca5a0b734d9fe9af66fbccd68b5fdfc4e0
Brent Garner <bgarner@gmail.com>
2016-02-03 15:49:47 -0700

    making a model

---
+++ app/Models/Notification/Notification.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Notification;
+
+use Illuminate\Database\Eloquent\Model;
+use Models\Document\Document
+
+class Notification extends Model
+{
+    public static function getAllNotifications($windowType, $windowSize)
+    {
+
+    }
+
+    public static function getNotificationsByFeature($windowType, $windowSize, $featureId)
+    {
+
+    }
+}
