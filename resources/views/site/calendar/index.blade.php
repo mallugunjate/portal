cf864019ffa89b25ff27c6a484fe38ed593c81ef
Megha Jyoti <jyoti5337@gmail.com>
2016-02-16 15:20:06 -0700

    showing event description in modal

--- resources/views/site/calendar/index.blade.php
+++ resources/views/site/calendar/index.blade.php
@@ -4,6 +4,7 @@
 <head>
     @section('title', 'Calendar')
     @include('site.includes.head')
+    <link rel="stylesheet" type="text/css" href="/css/custom/site/event.css">
 </head>	
 
 <body class="fixed-navigation">
@@ -45,6 +46,20 @@
                     </div>
                 </div>
          </div>
+         <div id="fullCalModal" class="modal fade">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                        <div class="modal-header">
+                            
+                            <h4 id="modalTitle" class="modal-title"></h4>
+                        </div>
+                        <div id="modalBody" class="modal-body"></div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                </div>
+            </div>
+        </div>
 {{-- 
          @foreach($events as $event)
             {{ $event->id }} - {{ $event->banner }} - {{ $event->title }} - {{$event->start}}::{{$event->end}}<br />
@@ -77,13 +92,22 @@
             defaultDate: today,
             editable: true,
             eventLimit: true, // allow "more" link when too many events
-            
+            eventClick:  function(event, jsEvent, view) {
+                console.log(event.start);
+                $('#modalTitle').html("<span class='event-title'>" + event.title +"</span>");
+                $('#modalTitle').append("<span class='event-span'>" + event.prettyStart+ " to " + event.prettyEnd + "</span>");
+                $('#modalBody').html(event.description);
+                $('#fullCalModal').modal();
+            },
             events: [
                 @foreach($events as $event)
                 {
                 title: "{{ $event->title }}",
                 start: "{{ $event->start }}",
-                end: "{{ $event->end }}"
+                end: "{{ $event->end }}",
+                description : "{{ $event->description }}",
+                prettyStart : "{{$event->prettyDateStart}}",
+                prettyEnd : "{{$event->prettyDateEnd}}",
                 },
                 @endforeach
             ]
@@ -92,6 +116,7 @@
     </script>
 
     @include('site.includes.bugreport')
+    
 
 </body>
 </html>                
