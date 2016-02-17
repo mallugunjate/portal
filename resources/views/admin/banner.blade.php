7e5b38d57739fba660198697fc527549f57a1c9b
Megha Jyoti <jyoti5337@gmail.com>
2016-02-17 10:22:55 -0700

    updated banner template

--- resources/views/admin/banner.blade.php
+++ resources/views/admin/banner.blade.php
@@ -1,27 +1,13 @@
-{{-- <div class="container-fluid">
-	<div class="navbar-header">
-		<a class="navbar-brand">          
-			<span></span>
-		</a>
-	</div>
+
+<li class="navbar-brand text-normal">
     
-    <ul class="nav navbar-nav"> --}}
-      	<li class="navbar-brand text-normal">
-            
-		</li>
-		
-		<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Banner <span class="caret"></span></a>
-			<ul class="dropdown-menu">
-				@foreach($banners as $banner)
-					<li> {{-- <a class="banner-switch" data-banner-id ={{$banner->id}}> {{$banner->name}} </a> --}} </li>
-				@endforeach
-			</ul>
-		</li>
-{{-- 	</ul> --}}
-	
-{{--     <ul class="nav navbar-nav navbar-right">
-          <li><a href="/admin/logout">Logout</a></li>
-    </ul>
-      
-</div> --}}
+</li>
+
+<li class="dropdown">
+	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Banner <span class="caret"></span></a>
+	<ul class="dropdown-menu">
+		@foreach($banners as $banner)
+			<li> <a class="banner-switch" data-banner-id ={{$banner->id}}> {{$banner->name}} </a>  </li>
+		@endforeach
+	</ul>
+</li>
