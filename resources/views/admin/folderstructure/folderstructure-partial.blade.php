e3379e900a6923eeea2eed17a0f98905eff0a15b
Megha Jyoti <jyoti5337@gmail.com>
2016-02-18 10:56:48 -0700

    folder structure ui updated for admin - #72

--- resources/views/admin/folderstructure/folderstructure-partial.blade.php
+++ resources/views/admin/folderstructure/folderstructure-partial.blade.php
@@ -1,13 +1,15 @@
 
 @if(count($nav["children"]) >0 )
-	<li class="folder" id="folder{{$nav['id']}}" data-id="{{$nav['id']}}" data-isWeek = false>
+	<li class="parent-folder folder " id="folder{{$nav['id']}}" data-id="{{$nav['id']}}" data-isWeek = false>
 
 		{{-- <a class="editFolder fa fa-pencil" href="/admin/folder/{{$nav['id']}}/edit" > --}}
-		<i class="fa fa-folder"></i>
-		<a class="modal-link" data-toggle="modal" data-remote="true" href="/admin/folder/{{$nav['id']}}/edit" data-target="#mmmm-modal">
+		
+		
 			<div class="folder-name">
 				{{$nav["label"]}}
 			</div>
+	 	<a class="modal-link" data-toggle="modal" data-remote="true" href="/admin/folder/{{$nav['id']}}/edit" data-target="#mmmm-modal">
+	 		<i class=" fa fa-pencil"></i>
 	 	</a>
 		<ul>
 
@@ -20,20 +22,22 @@
 	</li>
 
 @elseif ( isset($nav["weeks"]) && count($nav["weeks"] > 0) )
-	<li class="folder" id="folder{{$nav['id']}}" data-id="{{$nav['id']}}" data-isWeek="false"> 
+	<li class="parent-folder folder " id="folder{{$nav['id']}}" data-id="{{$nav['id']}}" data-isWeek="false"> 
 		{{-- <a class="editFolder fa fa-pencil" href="/admin/folder/{{$nav['id']}}/edit" > --}}
-		<i class="fa fa-folder"></i>
+		
+		
+		<div class="folder-name">
+			{{$nav["label"]}}
+		</div>
 		<a class="modal-link" data-toggle="modal" data-remote="true" href="/admin/folder/{{$nav['id']}}/edit" data-target="#mmmm-modal">
-			<div class="folder-name">
-				{{$nav["label"]}}
-			</div>
+			<i class="fa fa-pencil"></i>
 		</a>
-		<!-- <a class="deleteFolder" href="" data-folder-id={{$nav["id"]}} >Delete</a> -->
+		
 
 		<ul>
 			@foreach ($nav["weeks"]  as $week )
 			<li class="folder" id ="folder{{$week['week_id']}}" data-id="{{$week['week_id']}}" data-isWeek="true"> 
-				<ul class="fa folder-name">
+				<ul class="folder-name">
 					{{ "Week " . $week["week"] }}
 				</ul>
 			</li>
@@ -43,14 +47,17 @@
 	
 
 @else
-	<li class="folder" id="folder{{$nav['id']}}" data-id="{{$nav['id']}}" data-isWeek="false">
+	<li class="parent-folder folder " id="folder{{$nav['id']}}" data-id="{{$nav['id']}}" data-isWeek="false">
 		{{-- <a class="editFolder fa fa-pencil" href="/admin/folder/{{$nav['id']}}/edit" > --}}
-		<i class="fa fa-folder"></i>
-		<a class="modal-link" data-toggle="modal" data-remote="true" href="/admin/folder/{{$nav['id']}}/edit" data-target="#mmmm-modal">
+		
+		
 		<div class="folder-name"> 
 			{{$nav["label"]}}
 		</div>
+		
+		<a class="modal-link" data-toggle="modal" data-remote="true" href="/admin/folder/{{$nav['id']}}/edit" data-target="#mmmm-modal">
+			<i class=" fa fa-pencil"></i>
 		</a>
-		<!-- <a class="btn btn-default deleteFolder" data-folderId="{{$nav['id']}}">Delete</a> -->
+		<ul></ul>
 	</li>
 @endif
