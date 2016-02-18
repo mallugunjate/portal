ec7a746ff8fd30994c63068e28af5fe41dd9eeae
Brent Garner <bgarner@gmail.com>
2016-02-18 12:32:15 -0700

    fixes #114

--- resources/views/site/includes/modal.blade.php
+++ resources/views/site/includes/modal.blade.php
@@ -1,19 +1,14 @@
     <div class="modal inmodal" id="fileviewmodal" tabindex="-1" role="document" aria-hidden="true" style="display: none;">
+
+        <i class="fa fa-times-circle-o pull-right" data-dismiss="modal" style="font-size: 40px !important; color: #fff; cursor: pointer; padding: 20px;"></i>
+
         <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-                <!-- <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
-                    <i class="fa fa-bug modal-icon"></i>
-                    <h4 class="modal-title">Report a Bug</h4>
-                    <small class="font-bold">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</small>
-                </div> -->
+
                 <div class="modal-body">
 
-                <iframe src="" frameborder="0" height="100%" width="100%" allowtransparency="true"></iframe> 
-                <!--     <p><strong>Lorem Ipsum is simply dummy</strong> text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-                        printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                        remaining essentially unchanged.</p>
-                            <div class="form-group"><label>Sample Input</label> <input type="email" placeholder="Enter your email" class="form-control"></div> -->
+                    <iframe src="" frameborder="0" height="100%" width="100%" allowtransparency="true"></iframe> 
+            
                 </div>
 
             </div>
@@ -22,16 +17,14 @@
 
 
     <div class="modal inmodal" id="videomodal" tabindex="-1" role="video" aria-hidden="true" style="display: none;">
-        <div class="modal-dialog modal-lg" role="video">
-        <div class="modal-content videomodaltransparent">
-                <!-- <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
-                    <i class="fa fa-bug modal-icon"></i>
-                    <h4 class="modal-title">Report a Bug</h4>
-                    <small class="font-bold">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</small>
-                </div> -->
-                <div class="modal-body">
-                    <iframe frameborder="0" src="" frameborder="0" height="100%" width="100%" id="videoplayer" allowTransparency="true"></iframe>     
+
+        <i class="fa fa-times-circle-o pull-right" data-dismiss="modal" style="font-size: 40px !important; color: #fff; cursor: pointer; padding: 20px;"></i>
+
+        <div class="modal-dialog modal-lg" role="video" style="overflow: hidden;">
+        <div class="modal-content videomodaltransparent" style="overflow: hidden;">
+
+                <div class="modal-body" style="overflow: hidden;">
+                    <iframe frameborder="0" src="" frameborder="0" height="100%" width="100%" id="videoplayer" allowTransparency="true" style="overflow: hidden;"></iframe>     
                 </div>
 
             </div>
--- resources/views/site/urgentnotices/notice.blade.php
+++ resources/views/site/urgentnotices/notice.blade.php
@@ -185,26 +185,7 @@
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
-        });
-
-		$("body").on("click", ".launchPDFViewer", function(e){
-			var filepath = $(this).attr("data-file");
-			$("#fileviewmodal").find('iframe').attr("src", filepath);
-		});
-
-		$("body").on("click", ".launchVideoViewer", function(e){
-			var filepath = $(this).attr("data-file");
-			$("#videomodal").find('iframe').attr("src", "/video.php?v="+filepath);
-			$('#videomodal').modal('show');
-
-			$("#videomodal").find('iframe').css({backgroundColor: 'transparent'});
-			
-			// data-toggle="modal"
-		});
-
-		$('body').on('hidden.bs.modal', function () {
-		   $('iframe').attr('src', $('iframe').attr('src'));
-		});        
+        });     
     </script>
 <!--     <script type="text/javascript" src="/js/custom/site/communications/markAsRead.js"></script> -->
 
