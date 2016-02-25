dc28fdcb656be8a5ae6d502c41f93d1ae323d0e3
Megha Jyoti <jyoti5337@gmail.com>
2016-02-25 15:22:32 -0700

    display packages on communication

--- resources/views/site/feature/index.blade.php
+++ resources/views/site/feature/index.blade.php
@@ -154,24 +154,7 @@
                                             <div class="col-lg-4 package-listing">
                                             @foreach($feature_packages as $package)
 
-                                                <div class="feature_package" id="feature-package-{{$package->id}}" data-packageId = {{$package->id}}>
-                                                    <h4><i class="fa fa-gift"></i> {{$package->package_screen_name}} </h4>
-                                                    <?php  $package_folder_listing = $package['details']['package_folders']; ?>
-                                                    <?php  $package_folder_tree = $package['details']['package_folder_tree']; ?>
-                                                    <div class="package-folder-listing hidden" data-packageid= {{$package->id}}>
-                                                        
-                                                        @foreach ($package_folder_listing as $folder)
-                                                            <div class="package_folders" id="package-folder-{{$folder->global_folder_id}}" data-packageFolderId={{$folder->global_folder_id}}>
-                                                                <?php $folderstructure = ($package['details']['package_folder_tree'][$folder->global_folder_id]); ?>
-                                                                <?php $folder_root = ($package['details']['package_folder_tree'][$folder->global_folder_id][$folder->global_folder_id]); ?>
-                                                                <ul class="tree">
-                                                                @include('site.feature.folder-structure-partial', ['folderstructure' => $folderstructure, 'folder' =>$folder_root])
-                                                                </ul>
-                                                            </div>
-                                                            
-                                                        @endforeach
-                                                    </div>
-                                                </div>
+                                                @include('site.feature.package-listing', ['package'=>$package])
                                                 
                                             @endforeach
                                             </div>
