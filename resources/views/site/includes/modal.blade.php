4318b15d0035c9c3fd815be79351ee5aa935a752
Brent Garner <bgarner@gmail.com>
2016-03-09 17:04:19 -0700

    removed some lipsum

--- resources/views/site/includes/modal.blade.php
+++ resources/views/site/includes/modal.blade.php
@@ -58,7 +58,7 @@
                 <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
                 <i class="fa fa-bug modal-icon"></i>
                 <h4 class="modal-title">Report a Bug</h4>
-                <small class="font-bold">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</small>
+  <!--               <small class="font-bold">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</small> -->
             </div>
 
             <div class="modal-body" style="padding: 10px 10px;">
