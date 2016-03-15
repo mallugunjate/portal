88d8b830e8d3132231cd8f6f1bd6999ef85e69e8
Megha Jyoti <jyoti5337@gmail.com>
2016-03-15 10:36:23 -0600

    another possible place for the 0s in the dates to sneak in

--- app/Models/Alert/Alert.php
+++ app/Models/Alert/Alert.php
@@ -110,6 +110,7 @@ class Alert extends Model
                         ->where('alerts_target.store_id', '=', $store_id)
                         ->where('alerts.alert_end', '<=', Carbon::now()->toDatetimeString())
                         ->where('alerts.alert_end', '!=', '0000-00-00 00:00:00')
+                        ->select('alerts.*')
                         ->get();
 
 
@@ -132,6 +133,7 @@ class Alert extends Model
                         ->where('alerts.alert_end', '<=', Carbon::now()->toDatetimeString())
                         ->where('alerts.alert_end', '!=', '0000-00-00 00:00:00')
                         ->where('alert_type_id' , $alert_type)
+                        ->select('alerts.*')
                         ->get();
 
         if (count($alerts) >0) {
