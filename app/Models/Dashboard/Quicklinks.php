6ea8c9e1cfc732d208fd8fc18baa10b69dd7b334
Brent Garner <bgarner@gmail.com>
2016-03-15 16:35:13 -0600

    done the right way

--- app/Models/Dashboard/Quicklinks.php
+++ app/Models/Dashboard/Quicklinks.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use DB;
 use App\Models\Dashboard\QuicklinkTypes;
 use App\Models\Document\Document;
+use App\Models\Utility\Utility;
 
 class Quicklinks extends Model
 {
@@ -21,77 +22,12 @@ class Quicklinks extends Model
 
  			switch($link->type){
  				case 1: //folder
- 					array_push($linkarray, '<a href="/'.$storeNumber.'/document#!/'.$link->url.'" class="client-link"><i class="fa fa-folder-open"></i> '.$link->link_name.'</a>');
+ 					array_push($linkarray, '<a href="/'.$storeNumber.'/document#!/'.$link->url.'"><i class="fa fa-folder"></i> '.$link->link_name.'</a>');
  					break;
 
  				case 2: //file
  					$doc = Document::getDocumentById($link->url);
- 					$icon = "";
- 					$linkUrl = "";
- 					switch($doc->original_extension){
-	                    case "png":
-	                    case "jpg":
-	                    case "gif":
-	                    case "bmp":
-	                        $icon = "fa-file-image-o";              
-	                        $linkUrl = '<a href="#">';
-	                        break;
-
-	                    case "pdf":
-	                        $icon = "fa-file-pdf-o";
-	                        $linkUrl = '<a href="#" class="launchPDFViewer" data-toggle="modal" data-file="/viewer/?file=/files/'.$doc->filename.'" data-target="#fileviewmodal" class="client-link"> ';
-	                        break;
-
-	                    case "xls":
-	                    case "xlsx":
-	                        $icon = "fa-file-excel-o";
-	                        $linkUrl = '<a href="#">';
-	                        break;
-
-	                    case "mp4":
-	                    case "avi":
-	                    case "mov":
-	                        $icon = "fa-film";
-	                        $linkUrl = '<a href="#" class="launchVideoViewer" data-file="'.$doc->filename.'" data-target="#videomodal"> ';
-	                        break;
-
-	                    case "doc":
-	                    case "docx":
-	                        $icon = "fa-file-word-o";
-	                        $linkUrl = '<a href="#">';
-	                        break;
-
-	                    case "mp3":
-	                    case "wav":
-	                        $icon = "fa-file-audio-o";
-	                        $linkUrl = '<a href="#">';
-	                        break;
-
-	                    case "ppt":
-	                    case "pptx":
-	                        $icon = "fa-file-powerpoint-o";
-	                        $linkUrl = '<a href="#">';
-	                        break;
-
-	                    case "zip":
-	                        $icon = "fa-file-archive-o";
-	                        $linkUrl = '<a href="#">';
-	                        break;
-
-	                    case "html":
-	                    case "css":
-	                    case "js":
-	                        $icon = "fa-file-code-o";
-	                        $linkUrl = '<a href="#">';
-	                        break;
-	                        
-	                    default: 
-	                        $icon = "fa-file-o";
-	                        $linkUrl = '<a href="#">';
-	                        break;  
- 					}
-
- 					$finallink = $linkUrl .' <i class="fa '.$icon.'"></i>&nbsp;'. $link->link_name.'</a>';
+ 					$finallink = Utility::getModalLink($doc->filename, $doc->title, $doc->original_extension, 1);
  					array_push($linkarray, $finallink);
  					break; 					
 
