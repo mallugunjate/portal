f840ad8e9ca033a37e514355998ab4c564a860fe
Brent Garner <bgarner@gmail.com>
2016-03-17 17:11:10 -0600

    packages now come to the view in order

--- app/Models/Feature/FeaturePackage.php
+++ app/Models/Feature/FeaturePackage.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class FeaturePackage extends Model
 {
     protected $table = 'feature_package';
-    protected $fillable = ['feature_id', 'package_id'];
+    protected $fillable = ['feature_id', 'package_id', 'order'];
 }
