156c0e42bcdc7c797b8c5f5e17599f4dbad180b5
Megha Jyoti <jyoti5337@gmail.com>
2016-03-29 16:55:48 -0600

    added softDeletes to packages

--- app/Models/Document/DocumentPackage.php
+++ app/Models/Document/DocumentPackage.php
@@ -3,9 +3,12 @@
 namespace App\Models\Document;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DocumentPackage extends Model
 {
+    use SoftDeletes;
     protected $table = 'document_package';
     protected $fillable = ['document_id', 'package_id'];
+    protected $dates = ['deleted_at'];
 }
