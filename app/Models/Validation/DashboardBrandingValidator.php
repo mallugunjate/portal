7b7293408fc9c45aa59ab592dea85e63275641cb
Megha Jyoti <jyoti5337@gmail.com>
2016-04-11 13:28:13 -0600

    adding dashboard validation rules

--- app/Models/Validation/DashboardBrandingValidator.php
+++ app/Models/Validation/DashboardBrandingValidator.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class DashboardBrandingValidator extends Model
 {
-    //
+    protected $rules = [
+    	'title' => 'sometimes|required',
+    	'background' => 'sometimes|required|mimes:jpeg,jpf,png,bmp,gif',
+    	'update_type_id' => 'sometimes|in:1,2',
+    	'update_window_size' => 'sometimes|integer|min:1'
+
+    ];
 }
