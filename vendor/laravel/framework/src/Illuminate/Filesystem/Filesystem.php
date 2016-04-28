01ccedfcbb623a9f67cf86d6e393e5d9a19c13e3
Brent Garner <bgarner@gmail.com>
2016-04-28 12:04:11 -0600

    composer udpate

--- vendor/laravel/framework/src/Illuminate/Filesystem/Filesystem.php
+++ vendor/laravel/framework/src/Illuminate/Filesystem/Filesystem.php
@@ -78,7 +78,7 @@ class Filesystem
      */
     public function put($path, $contents, $lock = false)
     {
-        return file_put_contents($path, $contents, $lock ? LOCK_SH : 0);
+        return file_put_contents($path, $contents, $lock ? LOCK_EX : 0);
     }
 
     /**
