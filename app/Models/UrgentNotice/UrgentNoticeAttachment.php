b4d4b657d3b2e5ef6d11dd71e7f3fd88f4aa50b2
Megha Jyoti <jyoti5337@gmail.com>
2016-05-20 08:59:47 -0600

    deleting a folde now deletes its reference from parent containers

--- app/Models/UrgentNotice/UrgentNoticeAttachment.php
+++ app/Models/UrgentNotice/UrgentNoticeAttachment.php
@@ -10,12 +10,12 @@ class UrgentNoticeAttachment extends Model
     protected $table = 'urgent_notice_attachment';
     protected $fillable = ['urgent_notice_id' , 'attachment_id'];
 
-    public static function deleteAttachment($id, $type)
+    public static function deleteAttachment($attachment_id, $attachment_type)
     {
-    	$attachmentTypeId = UrgentNoticeAttachmentType::where('name', $type)->first()->id;
+    	$attachmentTypeId = UrgentNoticeAttachmentType::where('name', $attachment_type)->first()->id;
 
     	$urgentNoticeAttachmentRecordIds  = UrgentNoticeAttachment::join('urgent_notices', 'urgent_notice_attachment.urgent_notice_id', '=', 'urgent_notices.id')
-    										->where('urgent_notice_attachment.attachment_id', $id)
+    										->where('urgent_notice_attachment.attachment_id', $attachment_id)
     										->where('urgent_notices.attachment_type_id', $attachmentTypeId)
     										->select('urgent_notice_attachment.*')	
     										->get();
