77bafbfd150b7c157e11b94f4cb494bdb7973b19
Brent Garner <bgarner@gmail.com>
2016-06-14 09:47:53 -0600

    added list view tab

--- resources/views/site/calendar/index.blade.php
+++ resources/views/site/calendar/index.blade.php
@@ -34,6 +34,54 @@
         </div>
 
 
+                    <div class="tabs-container wrapper wrapper-content animated fadeInRight">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a data-toggle="tab" href="#tab-1" aria-expanded="true">Calendar View</a></li>
+                            <li class=""><a data-toggle="tab" href="#tab-2" aria-expanded="false">Event List</a></li>
+                        </ul>
+                        <div class="tab-content">
+                            <div id="tab-1" class="tab-pane active">
+                                <div class="panel-body">
+
+                                    <div class="ibox-content">
+                                        <div id="calendar"></div>                        
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div id="tab-2" class="tab-pane">
+                                <div class="panel-body">
+                                  
+                                  <div class="ibox-content inspinia-timeline" style="display: block;">
+
+                                        @foreach($events as $event)
+                                        <div class="timeline-item">
+                                            <div class="row">
+                                                <div class="col-xs-4 date">
+                                                    <i class="fa fa-calendar"></i>
+                                                    {!! $event->prettyDateStart !!} 
+                                                    <br>
+                                                    <small class="text-navy">2 hour ago</small>
+                                                </div>
+                                                <div class="col-xs-8 content">
+                                                    <p class="m-b-xs"><strong>{!! $event->title !!}</strong></p>
+                                                    <p>{!! $event->description !!}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        @endforeach
+
+                                    </div> 
+
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+{{--
          <div class="wrapper wrapper-content animated fadeInRight">
                 <div class="row">
                     <div class="col-lg-12">
@@ -47,7 +95,7 @@
                     </div>
                 </div>
          </div>
-
+--}}
 
 
     @include('site.includes.footer')       
@@ -70,7 +118,7 @@
             header: {
                 left: 'prev,next today',
                 center: 'title',
-                right: 'month,agendaWeek,agendaDay'
+                right: 'month,agendaWeek,agendaDay,list'
             },
             defaultDate: today,
             editable: true,
