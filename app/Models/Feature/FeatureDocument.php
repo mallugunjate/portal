1875edff83da575a907fb9de6198950bcfdd8141
Megha Jyoti <jyoti5337@gmail.com>
2016-06-24 15:06:54 -0600

    showing only active documents for features

--- app/Models/Feature/FeatureDocument.php
+++ app/Models/Feature/FeatureDocument.php
@@ -4,6 +4,9 @@ namespace App\Models\Feature;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Document\Document;
+use App\Models\Utility\Utility;
+use Carbon\Carbon;
 
 class FeatureDocument extends Model
 {
@@ -12,4 +15,29 @@ class FeatureDocument extends Model
     protected $fillable = ['document_id', 'feature_id'];
     protected $dates = ['deleted_at'];
     
+    public static function getFeaturedDocuments($feature_id)
+    {
+    	
+    	$now = Carbon::now()->toDatetimeString();
+    	$featuredDocuments = FeatureDocument::join('documents', 'feature_document.document_id', '=', 'documents.id')
+    							->where('feature_id', $feature_id)
+    							->where('documents.start', '<=', $now )
+	                            ->where(function($query) use ($now) {
+	                                $query->where('documents.end', '>=', $now)
+	                                    ->orWhere('documents.end', '=', '0000-00-00 00:00:00' ); 
+	                            })
+    							->select('documents.*')
+    							->get()
+    							->each(function($doc){
+
+    								$doc->folder_path   = Document::getFolderPathForDocument($doc->id);
+            						$doc->link          = Utility::getModalLink($doc->filename, $doc->title, $doc->original_extension, 0);
+            						$doc->link_with_icon= Utility::getModalLink($doc->filename, $doc->title, $doc->original_extension, 1);
+            						$doc->icon          = Utility::getIcon($doc->original_extension);
+            						$doc->prettyDate = Utility::prettifyDate($doc->updated_at);
+            						$doc->since = Utility::getTimePastSinceDate($doc->updated_at);
+
+    							});
+    	return $featuredDocuments;
+    }
 }
