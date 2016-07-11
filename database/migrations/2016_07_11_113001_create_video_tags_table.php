c9bdf6c93bf0440755f921db43b413fca182a713
Megha Jyoti <jyoti5337@gmail.com>
2016-07-11 11:46:27 -0600

    added migrations

---
+++ database/migrations/2016_07_11_113001_create_video_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_tags', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('video_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+            $table->foreign('video_id')->references('id')->on('videos')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('video_tags');
+    }
+}
---
+++ database/migrations/2016_07_11_113806_create_playlist_video_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlaylistVideoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('playlists_video', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('playlist_id')->unsigned();
+            $table->integer('video_id')->unsigned();
+            $table->foreign('playlist_id')->references('id')->on('playlists')->onDelete('cascade');
+            $table->foreign('video_id')->references('id')->on('videos')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes ();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('playlists_video');
+    }
+}
