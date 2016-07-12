3d31cfd2a40323906f04ba08c1950f9009734a45
Brent Garner <bgarner@gmail.com>
2016-07-12 10:08:26 -0600

    webm video

--- resources/views/site/video/index.blade.php
+++ resources/views/site/video/index.blade.php
@@ -61,7 +61,33 @@
 
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>This is the video title</h1>
+
+                    <video controls="controls" poster="/video/blink.jpg" style="">
+                        <source src="/video/blink.webm" type="video/webm" />
+                    </video>
+
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title clearfix">
+                            <h1 class="pull-left">This is the video title
+                                <br />
+
+
+                            </h1>
+                            <h2 class="pull-right">867,537 views<br />
+                                <div class="progress progress-mini" style="margin-bottom: 10px;">
+                                        <div style="width: 79%;" class="progress-bar"></div>
+                                </div>
+                                <button class="btn btn-white btn-outline" type="button" data-toggle="tooltip" data-placement="bottom" title="Like this"><i class="fa fa-thumbs-up"></i> 1</button>
+                                <button class="btn btn-white btn-outline" type="button" data-toggle="tooltip" data-placement="bottom" title="Dislike this"><i class="fa fa-thumbs-down"></i> 0</button>
+
+                            </h2>
+
+                        </div>
+                        <div class="ibox-content clearfix">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
