2a11164823e215eaec89790af14df6f9d201871e
Brent Garner <bgarner@gmail.com>
2016-08-16 14:54:08 -0600

    swapped the video and recent uploads on the dashbaord

--- resources/views/site/dashboard/index.blade.php
+++ resources/views/site/dashboard/index.blade.php
@@ -79,37 +79,43 @@
                     @endif
                 </div>
 
+                @if(isset($featuredVideo->id))
                 <div class="col-lg-4">
                     <div class="ibox float-e-margins">
                         <div class="ibox-title">
-                            <h2>Recent Uploads</h2>
+                            <h2>Featured Video</h2>
                         </div>
 
-                        <div class="ibox-content" style="max-height: 550px; overflow: auto;">
+                        <a href="/{{ Request::segment(1) }}/video/watch/{{$featuredVideo->id}}"><img src="/video/thumbs/{{$featuredVideo->thumbnail}}" data-video-id="{{$featuredVideo->id}}" class="trackclick img-responsive" style="width: 100%" /></a>
 
-                                <div class="feed-activity-list">
+                        <div class="ibox float-e-margins">
+                            <div class="ibox-title clearfix">
+                                <div class="pull-left">
+                                    <h3><a href="/{{ Request::segment(1) }}/video/watch/{{$featuredVideo->id}}" class="trackclick" data-video-id="{{$featuredVideo->id}}">{{$featuredVideo->title}}</a></h3>
+                                    <p>{{$featuredVideo->views}} views &middot; {{$featuredVideo->sinceCreated}} ago</p>
+                                    {{-- <h6>Tags:</h6>
+                                    <span class="label">SOmething</span>
+                                    <span class="label">SOm3thing totally different</span> --}}
+                                </div>
 
-                                @if(count($notifications)>0)
-                                    @foreach($notifications as $n)
-                                    <div class="feed-element">
-                                        <div class="media-body">
-                                            <span class="pull-left" style="padding: 0px 10px 0px 0px;">
-                                                <h2 style="padding: 0; margin: 0;">{!! $n->linkedIcon !!}</h2>
-                                            </span>
-                                            <small class="pull-right" style="padding-left: 10px;">{{ $n->since }} ago</small>
-                                                <strong>{!! $n->link !!}</strong> was {{ $n->verb }} <strong><a href="/{{ Request::segment(1) }}/document#!/{{ $n->global_folder_id }}">{{ $n->folder_name}}</a></strong>
-                                                @if($n->count > 1)
-                                                with <strong>{!! $n->count -1 !!}</strong> other documents
-                                                @endif
-                                        </div>
-                                    </div>
-                                    @endforeach
-                                @endif
+                                <div class="pull-right">
 
                                 </div>
+
                             </div>
+                            <div class="ibox-content clearfix">
+                                <p>{{$featuredVideo->description}} </p>
+                            </div>
+                        </div>
+
+
                     </div>
+
                 </div>
+
+            @endif
+
+
             </div>
 
             <div class="row">
@@ -172,41 +178,38 @@
 
                 </div>
 
-                @if(isset($featuredVideo->id))
-                <div class="col-lg-4">
-                    <div class="ibox float-e-margins">
-                        <div class="ibox-title">
-                            <h2>Featured Video</h2>
-                        </div>
 
-                        <a href="/{{ Request::segment(1) }}/video/watch/{{$featuredVideo->id}}"><img src="/video/thumbs/{{$featuredVideo->thumbnail}}" data-video-id="{{$featuredVideo->id}}" class="trackclick img-responsive" style="width: 100%" /></a>
-
-                        <div class="ibox float-e-margins">
-                            <div class="ibox-title clearfix">
-                                <div class="pull-left">
-                                    <h3><a href="/{{ Request::segment(1) }}/video/watch/{{$featuredVideo->id}}" class="trackclick" data-video-id="{{$featuredVideo->id}}">{{$featuredVideo->title}}</a></h3>
-                                    <p>{{$featuredVideo->views}} views &middot; {{$featuredVideo->sinceCreated}} ago</p>
-                                    {{-- <h6>Tags:</h6>
-                                    <span class="label">SOmething</span>
-                                    <span class="label">SOm3thing totally different</span> --}}
-                                </div>
-
-                                <div class="pull-right">
-
-                                </div>
-
-                            </div>
-                            <div class="ibox-content clearfix">
-                                <p>{{$featuredVideo->description}} </p>
-                            </div>
-                        </div>
-
-
+            <div class="col-lg-4">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-title">
+                        <h2>Recent Uploads</h2>
                     </div>
 
-                </div>
+                    <div class="ibox-content" style="max-height: 328px; overflow: auto;">
 
-            @endif
+                            <div class="feed-activity-list">
+
+                            @if(count($notifications)>0)
+                                @foreach($notifications as $n)
+                                <div class="feed-element">
+                                    <div class="media-body">
+                                        <span class="pull-left" style="padding: 0px 10px 0px 0px;">
+                                            <h2 style="padding: 0; margin: 0;">{!! $n->linkedIcon !!}</h2>
+                                        </span>
+                                        <small class="pull-right" style="padding-left: 10px;">{{ $n->since }} ago</small>
+                                            <strong>{!! $n->link !!}</strong> was {{ $n->verb }} <strong><a href="/{{ Request::segment(1) }}/document#!/{{ $n->global_folder_id }}">{{ $n->folder_name}}</a></strong>
+                                            @if($n->count > 1)
+                                            with <strong>{!! $n->count -1 !!}</strong> other documents
+                                            @endif
+                                    </div>
+                                </div>
+                                @endforeach
+                            @endif
+
+                            </div>
+                        </div>
+                </div>
+            </div>
 
             </div>
 
