c50dcd4afd7b056b54cbf617ae9e6c17858c6da8
Brent Garner <bgarner@gmail.com>
2016-08-23 10:32:19 -0600

    featured video doesnt lorem ipsum

--- resources/views/site/video/index.blade.php
+++ resources/views/site/video/index.blade.php
@@ -86,7 +86,7 @@
 
                             </div>
                             <div class="ibox-content clearfix">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                                <p>{{$featured->description}}</p>
                             </div>
                         </div>
                     </div>
